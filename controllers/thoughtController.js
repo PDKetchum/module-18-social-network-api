e5a9dcf59d85c467628fe4265f352e0325cd8a55
PDKetchum <pxiong265@gmail.com>
2022-11-16 22:51:48 -0600

    file clean up

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -7,7 +7,7 @@ module.exports = {
       .then((thoughts) => res.json(thoughts))
       .catch((err) => res.status(500).json(err));
   },
-  // Find otne thought
+  // Find one thought
   getSingleThought(req, res) {
     Thought.findOne({ _id: req.params.thoughtId })
       .select("-__v")
